e677344bca9549a1d238d3138d4f005a4b28f5ae
Veelkoov <veelkoov@protonmail.com>
2025-03-01 11:21:44 +0100

    Upgraded 3rd party

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,6 +8,7 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@PhpCsFixer' => true,
         '@PhpCsFixer:risky' => true,
+        'php_unit_method_casing' => false, // These are supposed to be descriptive, not pretty.
     ])
     ->setFinder($finder)
 ;
