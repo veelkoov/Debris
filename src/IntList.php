26f287e88d78fe9bf84579ea730500678ad68351
Veelkoov <veelkoov@protonmail.com>
2025-05-20 19:51:44 +0200

    Int set and list

---
+++ src/IntList.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Veelkoov\Debris;
+
+use Veelkoov\Debris\Base\DList;
+
+/**
+ * @extends DList<int>
+ */
+class IntList extends DList {}
---
+++ src/IntSet.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Veelkoov\Debris;
+
+use Veelkoov\Debris\Base\DSet;
+
+/**
+ * @extends DSet<int>
+ */
+class IntSet extends DSet {}
