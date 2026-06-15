ab7232d6ebf999a7aafaff1c4999dbc4d24c140b
Veelkoov <veelkoov@protonmail.com>
2026-06-15 22:38:22 +0200

    Revert "Look, an interface!"
    This reverts commit d3ef4dd2db1d5d4cd39b57e2575f72ffa67d7ba7.

--- src/Base/DMap.php
+++ src/Base/DMap.php
@@ -16,9 +16,10 @@ use Veelkoov\Debris\Maps\Pair;
  * @template K of object|scalar|null
  * @template V of object|scalar|null
  *
- * @implements Map<K, V>
+ * @implements Collection<K, V>
+ * @implements \Iterator<K, V>
  */
-class DMap implements Map
+class DMap implements Collection, \Iterator
 {
     protected readonly Freezer $freezer;
 
@@ -114,6 +115,9 @@ class DMap implements Map
     // ===== FREEZE ============================================
     //
 
+    /**
+     * @return $this
+     */
     public function freeze(): static
     {
         $this->freezer->freeze();
@@ -125,6 +129,9 @@ class DMap implements Map
     // ===== EMPTY AND COUNT ===================================
     //
 
+    /**
+     * @phpstan-assert-if-false non-empty-list<K> $this->getKeysArray()
+     */
     #[\Override]
     public function isEmpty(): bool
     {
@@ -147,6 +154,12 @@ class DMap implements Map
     // ===== ADD ===============================================
     //
 
+    /**
+     * @param K $key
+     * @param V $value
+     *
+     * @return $this
+     */
     public function set(mixed $key, mixed $value): static
     {
         $this->freezer->protect();
@@ -156,6 +169,11 @@ class DMap implements Map
         return $this;
     }
 
+    /**
+     * @param iterable<K, V> $items
+     *
+     * @return $this
+     */
     public function setAll(iterable $items): static
     {
         foreach ($items as $key => $value) {
@@ -169,11 +187,18 @@ class DMap implements Map
     // ===== PLUS ==============================================
     //
 
+    /**
+     * @param K $key
+     * @param V $value
+     */
     public function plus(mixed $key, mixed $value): static
     {
         return (new static($this))->set($key, $value);
     }
 
+    /**
+     * @param iterable<K, V> $items
+     */
     public function plusAll(iterable $items): static
     {
         return (new static($this))->setAll($items);
@@ -183,11 +208,21 @@ class DMap implements Map
     // ===== REMOVE ============================================
     //
 
+    /**
+     * @param V ...$value
+     *
+     * @return $this
+     */
     public function removeValue(mixed ...$value): static
     {
         return $this->removeAllValues($value);
     }
 
+    /**
+     * @param iterable<V> $values
+     *
+     * @return $this
+     */
     public function removeAllValues(iterable $values): static
     {
         $this->freezer->protect();
@@ -205,11 +240,21 @@ class DMap implements Map
         return $this;
     }
 
+    /**
+     * @param K ...$key
+     *
+     * @return $this
+     */
     public function removeKey(mixed ...$key): static
     {
         return $this->removeAllKeys($key);
     }
 
+    /**
+     * @param iterable<K> $keys
+     *
+     * @return $this
+     */
     public function removeAllKeys(iterable $keys): static
     {
         $this->freezer->protect();
@@ -225,21 +270,33 @@ class DMap implements Map
     // ===== MINUS =============================================
     //
 
+    /**
+     * @param V ...$value
+     */
     public function minusValue(mixed ...$value): static
     {
         return $this->minusAllValues($value);
     }
 
+    /**
+     * @param iterable<V> $values
+     */
     public function minusAllValues(iterable $values): static
     {
         return (new static($this))->removeAllValues($values);
     }
 
+    /**
+     * @param K ...$key
+     */
     public function minusKey(mixed ...$key): static
     {
         return $this->minusAllKeys($key);
     }
 
+    /**
+     * @param iterable<K> $keys
+     */
     public function minusAllKeys(iterable $keys): static
     {
         return (new static($this))->removeAllKeys($keys);
@@ -249,6 +306,9 @@ class DMap implements Map
     // ===== CONTAINS ==========================================
     //
 
+    /**
+     * @param V $value
+     */
     public function contains(mixed $value): bool
     {
         foreach ($this->items as $key) {
@@ -260,6 +320,9 @@ class DMap implements Map
         return false;
     }
 
+    /**
+     * @param K $key
+     */
     public function hasKey(mixed $key): bool
     {
         return $this->items->offsetExists($this->mappedKeys->get($key));
@@ -269,6 +332,9 @@ class DMap implements Map
     // ===== SORTED ============================================
     //
 
+    /**
+     * @param null|(callable(Pair<K, V>, Pair<K, V>): int)|(\Closure(Pair<K, V>, Pair<K, V>): int) $comparator
+     */
     public function sorted(callable|\Closure|null $comparator = null, bool $reverse = false): static
     {
         $times = $reverse ? -1 : 1;
@@ -346,6 +412,11 @@ class DMap implements Map
     // ===== ACCESSORS =========================================
     //
 
+    /**
+     * @param K $key
+     *
+     * @return V
+     */
     public function get(mixed $key): mixed
     {
         try {
@@ -361,6 +432,12 @@ class DMap implements Map
         }
     }
 
+    /**
+     * @param K                               $key
+     * @param (callable(): V)|(\Closure(): V) $newValueFunction
+     *
+     * @return V
+     */
     public function getOrSet(mixed $key, callable|\Closure $newValueFunction): mixed
     {
         if (!$this->hasKey($key)) {
@@ -370,6 +447,12 @@ class DMap implements Map
         return $this->get($key);
     }
 
+    /**
+     * @param K $key
+     * @param V $newValue
+     *
+     * @return V
+     */
     public function getOrSetAs(mixed $key, mixed $newValue): mixed
     {
         if (!$this->hasKey($key)) {
@@ -379,6 +462,14 @@ class DMap implements Map
         return $this->get($key);
     }
 
+    /**
+     * @template T
+     *
+     * @param K                                       $key
+     * @param (callable(): (T|V))|(\Closure(): (T|V)) $defaultValueFunction
+     *
+     * @return T|V
+     */
     public function getOrDefault(mixed $key, callable|\Closure $defaultValueFunction): mixed
     {
         if (!$this->hasKey($key)) {
@@ -388,6 +479,14 @@ class DMap implements Map
         return $this->get($key);
     }
 
+    /**
+     * @template T
+     *
+     * @param K   $key
+     * @param T|V $defaultValue
+     *
+     * @return T|V
+     */
     public function getOrDefaultOf(mixed $key, mixed $defaultValue): mixed
     {
         return $this->hasKey($key) ? $this->get($key) : $defaultValue;
@@ -397,6 +496,9 @@ class DMap implements Map
     // ===== FILTER ============================================
     //
 
+    /**
+     * @param (callable(K, V): bool)|(\Closure(K, V): bool) $filter
+     */
     public function filter(callable|\Closure $filter): static
     {
         $result = new static();
@@ -412,26 +514,41 @@ class DMap implements Map
         return $result;
     }
 
+    /**
+     * @param (callable(K, V): bool)|(\Closure(K, V): bool) $filter
+     */
     public function filterNot(callable|\Closure $filter): static
     {
         return $this->filter(static fn (mixed $key, mixed $value) => !$filter($key, $value));
     }
 
+    /**
+     * @param (callable(V): bool)|(\Closure(V): bool) $filter
+     */
     public function filterValues(callable|\Closure $filter): static
     {
         return $this->filter(static fn (mixed $key, mixed $value) => $filter($value));
     }
 
+    /**
+     * @param (callable(V): bool)|(\Closure(V): bool) $filter
+     */
     public function filterValuesNot(callable|\Closure $filter): static
     {
         return $this->filter(static fn (mixed $key, mixed $value) => !$filter($value));
     }
 
+    /**
+     * @param (callable(K): bool)|(\Closure(K): bool) $filter
+     */
     public function filterKeys(callable|\Closure $filter): static
     {
         return $this->filter(static fn (mixed $key, mixed $value) => $filter($key));
     }
 
+    /**
+     * @param (callable(K): bool)|(\Closure(K): bool) $filter
+     */
     public function filterKeysNot(callable|\Closure $filter): static
     {
         return $this->filter(static fn (mixed $key, mixed $value) => !$filter($key));
@@ -441,6 +558,9 @@ class DMap implements Map
     // ===== SINGLE ============================================
     //
 
+    /**
+     * @return Pair<K, V>
+     */
     public function single(): Pair
     {
         $key = $this->singleKey();
@@ -448,6 +568,9 @@ class DMap implements Map
         return new Pair($key, $this->get($key));
     }
 
+    /**
+     * @return V
+     */
     public function singleValue(): mixed
     {
         $key = $this->singleKey();
@@ -455,6 +578,9 @@ class DMap implements Map
         return $this->get($key);
     }
 
+    /**
+     * @return K
+     */
     public function singleKey(): mixed
     {
         if (1 !== $this->count()) {
@@ -470,6 +596,9 @@ class DMap implements Map
     // ===== RANDOM ============================================
     //
 
+    /**
+     * @return Pair<K, V>
+     */
     public function random(): Pair
     {
         $key = $this->randomKey();
@@ -478,11 +607,17 @@ class DMap implements Map
         return new Pair($key, $value);
     }
 
+    /**
+     * @return V
+     */
     public function randomValue(): mixed
     {
         return $this->get($this->randomKey());
     }
 
+    /**
+     * @return K
+     */
     public function randomKey(): mixed
     {
         if ($this->isEmpty()) {
@@ -498,6 +633,14 @@ class DMap implements Map
     // ===== MAP ===============================================
     //
 
+    /**
+     * @template OutV of object|scalar|null
+     * @template OutK of object|scalar|null
+     *
+     * @param (callable(K, V): array{OutK, OutV})|(\Closure(K, V): array{OutK, OutV}) $function
+     *
+     * @return static<OutK, OutV>
+     */
     public function map(callable|\Closure $function): static
     {
         return new static((function () use ($function) {
@@ -509,7 +652,17 @@ class DMap implements Map
         })());
     }
 
-    public function mapInto(callable|\Closure $function, Map $target): Map
+    /**
+     * @template OutV of object|scalar|null
+     * @template OutK of object|scalar|null
+     * @template Out of self<OutK, OutV>
+     *
+     * @param (callable(K, V): array{OutK, OutV})|(\Closure(K, V): array{OutK, OutV}) $function
+     * @param Out                                                                     $target
+     *
+     * @return Out
+     */
+    public function mapInto(callable|\Closure $function, self $target): self
     {
         foreach ($this as $key => $value) {
             $pair = $function($key, $value);
@@ -520,22 +673,54 @@ class DMap implements Map
         return $target;
     }
 
-    public function mapKeys(callable|\Closure $function): Map
+    /**
+     * @template OutK of object|scalar|null
+     *
+     * @param (callable(K): OutK)|(\Closure(K): OutK) $function
+     *
+     * @return self<OutK, V>
+     */
+    public function mapKeys(callable|\Closure $function): self
     {
         return $this->map(static fn (mixed $key, mixed $value) => [$function($key), $value]);
     }
 
-    public function mapKeysInto(callable|\Closure $function, Map $target): Map
+    /**
+     * @template OutK of object|scalar|null
+     * @template Out of self<OutK, V>
+     *
+     * @param (callable(K): OutK)|(\Closure(K): OutK) $function
+     * @param Out                                     $target
+     *
+     * @return Out
+     */
+    public function mapKeysInto(callable|\Closure $function, self $target): self
     {
         return $this->mapInto(static fn (mixed $key, mixed $value) => [$function($key), $value], $target);
     }
 
-    public function mapValues(callable|\Closure $function): Map
+    /**
+     * @template OutV of object|scalar|null
+     *
+     * @param (callable(V): OutV)|(\Closure(V): OutV) $function
+     *
+     * @return self<K, OutV>
+     */
+    public function mapValues(callable|\Closure $function): self
     {
         return $this->map(static fn (mixed $key, mixed $value) => [$key, $function($value)]);
     }
 
-    public function mapValuesInto(callable|\Closure $function, Map $target): Map
+    /**
+     * @template OutV of object|scalar|null
+     * @template Out of self<K, OutV>
+     *
+     * @param (callable(V): OutV)|(\Closure(V): OutV) $function
+     * @param Out                                     $target
+     *
+     * @return Out
+     */
+    public function mapValuesInto(callable|\Closure $function, self $target): self
     {
         return $this->mapInto(static fn (mixed $key, mixed $value) => [$key, $function($value)], $target);
     }
@@ -570,6 +755,9 @@ class DMap implements Map
     // ===== GET ARRAY =========================================
     //
 
+    /**
+     * @return list<K>
+     */
     public function getKeysArray(): array
     {
         $result = [];
@@ -581,6 +769,9 @@ class DMap implements Map
         return $result;
     }
 
+    /**
+     * @return list<V>
+     */
     public function getValuesArray(): array
     {
         $result = [];
@@ -592,16 +783,25 @@ class DMap implements Map
         return $result;
     }
 
+    /**
+     * @return DSet<K>
+     */
     public function getKeys(): DSet
     {
         return new DSet($this->getKeysArray());
     }
 
+    /**
+     * @return DList<V>
+     */
     public function getValues(): DList
     {
         return new DList($this->getValuesArray());
     }
 
+    /**
+     * @return list<Pair<K, V>>
+     */
     public function getPairsArray(): array
     {
         $result = [];
@@ -617,6 +817,9 @@ class DMap implements Map
     // ===== ANY ===============================================
     //
 
+    /**
+     * @param (callable(K, V): bool)|(\Closure(K, V): bool) $testFunction
+     */
     public function any(callable|\Closure $testFunction): bool
     {
         foreach ($this as $key => $value) {
@@ -628,11 +831,17 @@ class DMap implements Map
         return false;
     }
 
+    /**
+     * @param (callable(V): bool)|(\Closure(V): bool) $testFunction
+     */
     public function anyValue(callable|\Closure $testFunction): bool
     {
         return $this->any(static fn (mixed $key, mixed $value) => $testFunction($value));
     }
 
+    /**
+     * @param (callable(K): bool)|(\Closure(K): bool) $testFunction
+     */
     public function anyKey(callable|\Closure $testFunction): bool
     {
         return $this->any(static fn (mixed $key, mixed $value) => $testFunction($key));
@@ -642,6 +851,9 @@ class DMap implements Map
     // ===== ALL ===============================================
     //
 
+    /**
+     * @param (callable(K, V): bool)|(\Closure(K, V): bool) $testFunction
+     */
     public function all(callable|\Closure $testFunction): bool
     {
         foreach ($this as $key => $value) {
@@ -653,11 +865,17 @@ class DMap implements Map
         return true;
     }
 
+    /**
+     * @param (callable(V): bool)|(\Closure(V): bool) $testFunction
+     */
     public function allValues(callable|\Closure $testFunction): bool
     {
         return $this->all(static fn (mixed $key, mixed $value) => $testFunction($value));
     }
 
+    /**
+     * @param (callable(K): bool)|(\Closure(K): bool) $testFunction
+     */
     public function allKeys(callable|\Closure $testFunction): bool
     {
         return $this->all(static fn (mixed $key, mixed $value) => $testFunction($key));
@@ -704,6 +922,9 @@ class DMap implements Map
     // ===== OTHER ============================================
     //
 
+    /**
+     * @return static<V, K>
+     */
     public function flip(): static
     {
         $result = new static();
